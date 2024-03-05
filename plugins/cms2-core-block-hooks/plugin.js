3067a2271f64f290d425e6180254175da6175740
Julian Postolan <Julezp10@gmail.com>
2024-03-05 14:11:31 -0500

    Plugin Update

--- plugins/cms2-core-block-hooks/plugin.js
+++
@@ -1,9 +0,0 @@
-function myguten_enqueue() {
-   wp_enqueue_script(
-       'myguten-script',
-       plugins_url( 'myguten.js', __FILE__ ),
-       array( 'wp-blocks', 'wp-dom-ready', 'wp-edit-post' ),
-       filemtime( plugin_dir_path( __FILE__ ) . '/myguten.js' )
-   );
-}
-add_action( 'enqueue_block_editor_assets', 'myguten_enqueue' );
