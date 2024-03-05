acff662ab5ccee1cbba0c120e6fdf43777cc8091
Julian Postolan <Julezp10@gmail.com>
2024-03-05 14:33:40 -0500

    Core Block CSS
    bug fix not allowing proper targeting

--- themes/block-theme-costom/functions.php
+++ themes/block-theme-costom/functions.php
@@ -17,22 +17,10 @@
  */
 function block_theme_costom_styles() {
 	wp_enqueue_style(
-		'block-theme-costom-style',
-		get_stylesheet_uri(),
-		[],
-		wp_get_theme()->get( 'Version' )
+		 'block-theme-costom-style',
+		 get_stylesheet_directory_uri() . '/block-theme-costom-style.css',
+		 [],
+		 wp_get_theme()->get('Version')
 	);
-	// wp_enqueue_style(
-	// 	'bootstrap',
-	// 	get_template_directory_uri() . '/assets/css/bootstrap.min.css',
-	// 	[],
-	// 	'5.3.2'
-	// );
-	// wp_enqueue_script(
-	// 	'bootstrap',
-	// 	get_template_directory_uri() . '/assets/js/bootstrap.min.js',
-	// 	[],
-	// 	'5.3.2',
-	// );
 }
-add_action( 'wp_enqueue_scripts', 'block_theme_costom_styles' );
+add_action('wp_enqueue_scripts', 'block_theme_costom_styles');
